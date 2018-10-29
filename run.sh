9978da316472b20398ed247d101bc4fa09b8ed5b
Guido Vollbach <gvollbach@databay.de>
2018-10-29 11:25:32 +0100

    moved

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATE=`date '+%Y-%m-%d-%H:%M:%S'.txt`
-echo "Hello World" > "$DATE"
+echo "Hello World" > results/"$DATE"
 git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE auto commit"
 git remote add results https://${VOLLNIXX_VAR}@github.com/vollnixx/TravisResults.git > /dev/null 2>&1
 git push --quiet --set-upstream results master
