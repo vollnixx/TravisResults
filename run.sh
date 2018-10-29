86d33b1f7bc6e9ee831ace6ab6782e7c0a4930e9
Guido Vollbach <gvollbach@databay.de>
2018-10-29 13:33:48 +0100

    blubb

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DATE=`date '+%Y-%m-%d-%H:%M:%S'.txt`
-echo "Hello World" > results/"$DATE"
-git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE auto commit"
+DATE=`date '+%Y-%m-%d-%H:%M:%S'`
+echo "Hello World" > results/"$DATE.txt"
+git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE - $TRAVIS_EVENT_TYPE"
 git remote add results https://${VOLLNIXX_VAR}@github.com/vollnixx/TravisResults.git > /dev/null 2>&1
 git push --quiet --set-upstream results master
