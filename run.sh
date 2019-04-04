0ed3e50abf8999fcf0db8b4dfbf69a3699e2b725
Guido Vollbach <gvollbach@databay.de>
2019-04-04 10:30:29 +0200

    added dashboard files

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+DICTO_PATH="/tmp/dicto_latest.csv"
+PHPUNIT_PATH="/tmp/phpunit_latest.csv"
+
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
-echo "Hello World" > results/"$DATE.txt"
+echo "Hello World" > results/"phpunit_$TRAVIS_BUILD_NUMBER_$DATE.txt"
+if [ -e "$PHPUNIT_PATH_TMP" ]
+	then
+		mv "$PHPUNIT_PATH_TMP" "data"
+fi
+if [ -e "$DICTO_PATH" ]
+	then
+		mv "$DICTO_PATH" "data"
+fi
 git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE - $TRAVIS_EVENT_TYPE"
 git remote add results https://${VOLLNIXX_VAR}@github.com/vollnixx/TravisResults.git > /dev/null 2>&1
 git push --quiet --set-upstream results master
